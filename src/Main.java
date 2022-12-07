02c7a410072502b3e024b3c72a3844745645534e
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2022-12-07 20:00:29 +0800

    Update kmer distribution

--- src/Main.java
+++ src/Main.java
@@ -8,49 +8,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //try catch so that the input will only take the valid characters
         System.out.println("Input: ");
         String dna = input.nextLine();
         System.out.println("K-mer: ");
         int merDistribution = input.nextInt();
 
-        LinkedList<String>[] arr = new LinkedList[dna.length()];
+        LinkedList<String>[] hashTable = new LinkedList[dna.length()];
         ArrayList<Kmer> kmerDistribution = new ArrayList<Kmer>();
         int key;
-        String substring;
 
         //initialize the linkedlists
         for (int i = 0; i < dna.length(); i++) {
-            arr[i] = new LinkedList<String>();
+            hashTable[i] = new LinkedList<String>();
         }
 
+        //create hashtable
         for (int i = 0; i < dna.length() - merDistribution + 1 ; i++) {
-            //hash function
-            substring = dna.substring(i, i + merDistribution);
-            key = Math.abs(MurmurHash3.hash32x86(substring.getBytes()) % dna.length() - 1);
-            if (!arr[key].contains(substring)) kmerDistribution.add(new Kmer(substring));
-            arr[key].add(substring);
+            String substring = dna.substring(i, i + merDistribution);
+            key = Math.abs(MurmurHash3.hash32x86(substring.getBytes()) % dna.length());
+            hashTable[key].add(substring);
         }
 
-        for (Kmer k: kmerDistribution) {
-            boolean isFound = false;
-            for (int i = 0; i < arr.length && !isFound; i++) {
-                if (arr[i].contains(k.getName())) {
-                    for (int j = 0; j < arr[i].size(); j++) {
-                        if (arr[i].get(j).equals(k.getName())) k.setNumOccurrences(k.getNumOccurrences() + 1);
+        //create kmer distribution
+        for (int i = 0; i < hashTable.length; i++) {
+            for (int j = 0; j < hashTable[i].size(); j++) {
+                String substring = hashTable[i].get(j);
+                boolean isFound = false;
+                if (kmerDistribution.size() == 0) kmerDistribution.add(new Kmer(substring));
+                else {
+                    for (int k = 0; k < kmerDistribution.size() && !isFound; k++) {
+                        if (substring.compareTo(kmerDistribution.get(k).getName()) == 0) {
+                            kmerDistribution.get(k).setNumOccurrences();
+                            isFound = true;
+                        }
                     }
-                    isFound = true;
+                    if (!isFound) kmerDistribution.add(new Kmer(substring));
                 }
             }
         }
 
-        for (LinkedList s : arr) {
+
+
+        for (LinkedList s : hashTable) {
             System.out.println(s);
         }
 
         for (Kmer k: kmerDistribution) {
             System.out.println(k.getName() + " " + k.getNumOccurrences());
         }
-
     }
 }
+
+//taccaccaccatag
