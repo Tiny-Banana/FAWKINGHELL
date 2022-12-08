bef09f56d64f1e8b67af3a679b0fef5c84ff9b96
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2022-12-08 12:45:24 +0800

    Create hashtable class for instantiation

---
+++ src/HashTable.java
@@ -0,0 +1,48 @@
+import org.apache.commons.codec.digest.MurmurHash3;
+import java.util.LinkedList;
+
+public class HashTable {
+    LinkedList<Kmer>[] hashTable;
+    int size;
+
+    public HashTable(int size) {
+        this.hashTable = new LinkedList[size];
+        this.size = size;
+        intializeHashTable();
+    }
+
+    private void intializeHashTable() {
+        for (int i = 0; i < size; i++) {
+            hashTable[i] = new LinkedList<>();
+        }
+    }
+
+    public void insert(String key, int value) {
+        int index = Math.abs(MurmurHash3.hash32x86(key.getBytes()) % size);
+        boolean isFound = false;
+        for (int i = 0; i < hashTable[index].size() && !isFound; i++) {
+            Kmer kmer = hashTable[index].get(i);
+            if (kmer.getSubstring().compareTo(key) == 0) {
+                kmer.setNumOccurrences(kmer.getNumOccurrences()+ 1);
+                isFound = true;
+            }
+        }
+        if (!isFound) hashTable[index].add(new Kmer(key, value));
+    }
+
+    public int search(String key) {
+        for (int i = 0; i < hashTable.length; i++) {
+            for (int j = 0; j < hashTable[i].size(); j++) {
+                Kmer kmer = hashTable[i].get(j);
+                if (kmer.getSubstring().compareTo(key) == 0) return kmer.getNumOccurrences();
+            }
+        }
+        return 0;
+    }
+
+    public void print() {
+        for (LinkedList s : hashTable) {
+            System.out.println(s);
+        }
+    }
+}
