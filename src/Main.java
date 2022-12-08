270703db7a3a560caae76cc399a96ff353163a3b
Jomar Delos Reyes <jomardelosreyes32@gmail.com>
2022-12-08 12:46:55 +0800

    Update the class in accordance to hashtable class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
-
-import org.apache.commons.codec.digest.MurmurHash3;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,57 +9,19 @@ public class Main {
         System.out.println("K-mer: ");
         int merDistribution = input.nextInt();
 
-        LinkedList<String>[] hashTable = new LinkedList[dna.length()];
-        ArrayList<Kmer> kmerDistribution = new ArrayList<Kmer>();
-
-        //initialize the linkedlists
-        for (int i = 0; i < dna.length(); i++) {
-            hashTable[i] = new LinkedList<String>();
-        }
-
+        HashTable hashTable = new HashTable(dna.length());
         createHashTable(dna, merDistribution, hashTable);
-        createKmerDistribution(hashTable, kmerDistribution);
-
-        for (LinkedList s : hashTable) {
-            System.out.println(s);
-        }
-
-        for (Kmer k: kmerDistribution) {
-            System.out.println(k.getName() + " " + k.getNumOccurrences());
-        }
+        //create kmerdistribution arraylist if needed
     }
-
-    public static void createHashTable(String dna, int merDistribution, LinkedList<String>[] hashTable) {
+    public static void createHashTable(String dna, int merDistribution, HashTable hashTable) {
         //create hashtable
-        int index;
         for (int i = 0; i < dna.length() - merDistribution + 1 ; i++) {
             String substring = dna.substring(i, i + merDistribution);
-            index = Math.abs(MurmurHash3.hash32x86(substring.getBytes()) % dna.length());
-            hashTable[index].add(substring);
+            hashTable.insert(substring, 1);
         }
-    }
-    public  static void createKmerDistribution(LinkedList<String>[] hashTable, ArrayList<Kmer> kmerDistribution) {
-        //create kmer distribution
-        for (int i = 0; i < hashTable.length; i++) {
-            for (int j = 0; j < hashTable[i].size(); j++) {
-                String substring = hashTable[i].get(j);
-                boolean isFound = false;
-                if (kmerDistribution.size() == 0) kmerDistribution.add(new Kmer(substring));
-                else {
-                    for (int k = 0; k < kmerDistribution.size() && !isFound; k++) {
-                        if (substring.compareTo(kmerDistribution.get(k).getName()) == 0) {
-                            kmerDistribution.get(k).setNumOccurrences();
-                            isFound = true;
-                        }
-                    }
-                    if (!isFound) kmerDistribution.add(new Kmer(substring));
-                }
-            }
-        }
+
+        hashTable.print();
     }
 }
-
-
-
 
 //taccaccaccatag
